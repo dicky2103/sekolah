7a5a26619496a8673bc51e1ffafb0368b98a5732
dicky azhari hernuwan <hernuwandicky@gmail.com>
2023-04-22 12:24:28 +0700

    sekolah

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Rebahan School </title>
+    <link rel="icon" type="image/png" href="img/12.jpg">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet"> 
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><img  src="img/12.jpg" alt=""><a href="">Rebahan School</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#Kegiatan">Kegiatan</a></li>
+                    <li><a href="#Pengajar">Pengajar</a></li>
+                    <li><a href="#Tentang">Tentang</a></li>
+                    <li><a href="#Kontak">Kontak</a></li>
+                    <li><a href="#Kejuruan">Kejuruan</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <section id="Home">
+            <img src="img/4.jpg" class="ukuran">
+            <div class="kolom">
+                <p class="Deskripsi">Belajar Progam #dikamar aja </p>
+                <h2>Tetap Sehat Tetap Semangat Tetap Santuy</h2>
+                <p>Lurus Aqidahnya, Benar Ibadahnya, Baik Akhlaknya</p>
+                <p></p>
+                
+            </div>
+        </section>
+        
+        <section id="Kegiatan">
+            <div class="kolom">
+                <p><a href="" class="Deskripsi"></a></p>
+                <h2>Kegiatan</h2>
+               <p>proses pembelajaran murid murid rebahan school dimana mereka belajarnya masih tetap santuy dan nikmat, 
+                serta tidak ada beban dalam menjalan proses rebahannya</p>
+            </div>
+            <img src="img/kelas.jpg" class="uku">
+        </section>
+
+        <section id="Pengajar">
+            <div class="Tengah">
+                <div class="kolom">
+                    <p class="Deskripsi">Contoh Santuy</p>
+                    <h2>Pengajar</h2>
+                    <p>ini adalah salah contoh contoh pengajar santuy </p>
+                </div>
+                <div class="Pengajar-list"> 
+                    <div class="kartu-tutor">
+                        <img src="img/p1.jpeg" alt="">
+                        <p></p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="img/g1.jpg" alt="">
+                        <p>rizky(guru tergabut)</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="img/p1.jpeg" alt="">
+                        <p></p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="img/p1.jpeg" alt="">
+                        <p></p>
+                    </div>
+
+
+
+
+                </div>
+            </div>
+        </section>
+        <section id="Kejuruan">
+            <div class="Tengah">
+                <div class="kolom">
+                    <p class="Deskripsi">Kejuruan</p>
+                    <p>Memasang Kasur</p>
+                    <p>Membersihkan peralatan tidur</p>
+                    <p>Memperbaikan tempat tidur</p>
+                </div>
+            </div>
+        </section>
+
+        <section id="Tentang" >
+            <div class="Tengah">
+                <div class="kolom">
+                    <p class="Deskripsi"> Tentang</p>
+                    <h2>Visi</h2>
+                    <p>Membentuk Generasi Berakhlakul Karimah Berlandaskan Tauhid</p>
+                    <h2>Misi</h2>
+                    <p>Menyelenggarakan pendidikan Islam yang berkualitas berdasarkan Al Qur’an dan Sunnah sesuai pemahaman Salafus Sholih
+                       Menyelenggarakan pendidikan Islam yang professional berfokus pada pembinaan Aqidah, Akhlak dan Ibadah sesuai dengan sunnah Rosululloh ﷺ
+                       Menyelenggarakan pendidikan Islam yang mendorong peserta didik senantiasa berpikir ilmiah, kreatif dan eksploratif</p>
+                    <h2>Sejarah</h2>
+                    <p>Rebahan School berdiri pada tahun 2021 dimana sekolah ini telah dibuatkan penyebabnya yaitu ketika sekolah daring para siswa semuanya lebih menyukai
+                        rebahan dan pada santuy makanya sekolah ini dibuat dan dinamakan rebahan school
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div id="Kontak">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>Rebahan School</h3>
+                    <p>Kehidupan yang selalu santuy tanpa sebuah beban</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Alamat</h3>
+                    <p>Jl. Dunia Lain, Kawasan Global, Planet Bumi</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Kontak</h3>
+                    <p>email: rebahan@gmail.com</p>
+                    <p>+62856377214</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
